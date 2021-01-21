c7cc9d30875178f8a12a5ae8c42de05751ae1cd8
hdm007 <harry.mardirossian13@gmail.com>
2021-01-20 20:58:59 -0500

    Added axios call and useEffect, locked out of api for an hour

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import "./App.css";
+import axios from "axios";
+import Header from "./components/Header"
 
 function App() {
+  const [nasaInfo, setNasaInfo] = useState({})
+
+  useEffect( () => {
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY").then((res) =>{
+      setNasaInfo(res.data)
+    }).catch((res) =>{
+      console.log(res)
+    })},[])
   return (
     <div className="App">
+      <Header nasaInfo = {nasaInfo}/>
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
+       <span role="img" aria-label='go!'>🚀</span>!
       </p>
     </div>
   );
---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Header(props) {
+    const { nasaInfo } = props
+
+    return (
+        <header>
+            <h1>Placeholder for curl- nasaInfo.name -curl</h1>
+        </header>
+    )
+}
+
+export default Header;
